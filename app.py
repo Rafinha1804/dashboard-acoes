1a38ed98975ebb12b817db24ce03accdeb40d89c
Rafael <joaorafael.msantos@gmail.com>
2026-06-04 23:49:04 -0300

    auto: 2026-06-04 23:49

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template  # dashboard-acoes
 import plotly.express as px
 import plotly.graph_objects as go
 import pandas as pd
